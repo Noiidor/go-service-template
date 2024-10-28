11f48c6abc035745028fe3c7df185a45f0748e47
noi <noidor2019@gmail.com>
2024-10-29 00:58:54 +0700

    Implemented wizards repo

--- internal/repos/postgres/wizards.go
+++ internal/repos/postgres/wizards.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+var _ repos.WizardsRepo = (*wizardsRepo)(nil)
+
 type wizardsRepo struct {
 	db *sqlx.DB
 }
@@ -18,33 +20,104 @@ func NewWizardsRepo(db *sqlx.DB) *wizardsRepo {
 }
 
 func (r *wizardsRepo) GetWizardByID(ctx context.Context, id uint32) (*repos.Wizard, error) {
-	return nil, nil
+	wizard := new(repos.Wizard)
+	const query = `
+		SELECT id, name, specialization
+		FROM wizards
+		WHERE id = $1;
+	`
+
+	err := r.db.GetContext(ctx, wizard, query, id)
+	if err != nil {
+		return nil, err
+	}
+	return wizard, nil
 }
 
 func (r *wizardsRepo) GetAllWizards(ctx context.Context) ([]*repos.Wizard, error) {
-	return nil, nil
+	var wizards []*repos.Wizard
+	const query = `
+		SELECT id, name, specialization
+		FROM wizards;
+	`
+
+	err := r.db.SelectContext(ctx, &wizards, query)
+	if err != nil {
+		return nil, err
+	}
+	return wizards, nil
 }
 
 func (r *wizardsRepo) CreateWizard(ctx context.Context, wizard *repos.Wizard) error {
-	return nil
+	const query = `
+		INSERT INTO wizards (name, specialization)
+		VALUES (:name, :specialization)
+		RETURNING id;
+	`
+
+	rows, err := r.db.NamedQueryContext(ctx, query, wizard)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	rows.Scan(wizard.ID)
+
+	return err
 }
 
 func (r *wizardsRepo) UpdateWizard(ctx context.Context, wizard *repos.Wizard) error {
-	return nil
+	const query = `
+		UPDATE wizards 
+		SET name = $1, specialization = $2 
+		WHERE id = $3;
+	`
+
+	_, err := r.db.NamedExecContext(ctx, query, wizard)
+	return err
 }
 
 func (r *wizardsRepo) DeleteWizard(ctx context.Context, id uint32) error {
-	return nil
+	const query = `
+		DELETE FROM wizards 
+		WHERE id = $1;
+	`
+
+	_, err := r.db.ExecContext(ctx, query, id)
+	return err
 }
 
 func (r *wizardsRepo) AddStatsToWizard(ctx context.Context, stats *repos.WizardStats) error {
-	return nil
+	const query = `
+		INSERT INTO wizard_stats (wizard_id, power, mana, intelligence, luck) 
+		VALUES (:wizard_id, :power, :mana, :intelligence, :luck);
+	`
+
+	_, err := r.db.NamedExecContext(ctx, query, stats)
+	return err
 }
 
 func (r *wizardsRepo) GetWizardStats(ctx context.Context, wizardID uint32) (*repos.WizardStats, error) {
-	return nil, nil
+	stats := new(repos.WizardStats)
+	const query = `
+		SELECT wizard_id, power, mana, intelligence, luck 
+		FROM wizard_stats 
+		WHERE wizard_id = $1;
+	`
+
+	err := r.db.GetContext(ctx, stats, query, wizardID)
+	if err != nil {
+		return nil, err
+	}
+	return stats, nil
 }
 
 func (r *wizardsRepo) UpdateStats(ctx context.Context, wizardID uint32, stats *repos.WizardStats) error {
-	return nil
+	const query = `
+		UPDATE wizard_stats
+		SET power = :power, mana = :mana, intelligence = :intelligence, luck = :luck
+		WHERE wizard_id = :wizard_id;
+	`
+	_, err := r.db.NamedExecContext(ctx, query, stats)
+	return err
 }
